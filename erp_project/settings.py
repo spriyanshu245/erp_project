b0b017af53a3adbb11c1506a6d285f9150551ec9
sauravdharwadkar <dharwadkarsaurav@gmail.com>
2020-10-13 19:29:13 +0530

    changed mysql to sqlite3

--- erp_project/settings.py
+++ erp_project/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'erp_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
@@ -85,7 +85,13 @@ DATABASES = {
        
     }
 }
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
