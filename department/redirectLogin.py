4ea8408f30fca6380aa82533a38229949641a7f4
sauravdharwadkar <dharwadkarsaurav@gmail.com>
2021-02-28 10:55:49 +0530

    LoginPage redirect

--- department/redirectLogin.py
+++ department/redirectLogin.py
@@ -2,11 +2,11 @@
 from django.shortcuts import HttpResponseRedirect
 from django.utils.deprecation import MiddlewareMixin
 
-
+LOGIN_PAGE ="/loginPage/"
 class AuthRequiredMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        if request.get_full_path() == "/":
+        if request.get_full_path() == LOGIN_PAGE:
             pass
-        elif not request.user.is_authenticated:
-            return HttpResponseRedirect('/') # or http response
+        elif not request.user.is_authenticated :
+            return HttpResponseRedirect(LOGIN_PAGE) # or http response
         return None
